835d55b221346e90d6ed2db7e25ce026ece861bd
nathanziarczyk <nathanziarczyk1997@outlook.com>
2020-05-15 14:41:02 +0200

    env prod voor auth in production

--- public/index.php
+++ public/index.php
@@ -5,10 +5,10 @@ use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 // TODO DEV
-//require dirname(__DIR__).'/config/bootstrap.php';
+require dirname(__DIR__).'/config/bootstrap.php';
 
 // TODO PROD
-require 'system/config/bootstrap.php';
+//require 'system/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
